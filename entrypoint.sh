1f50ee3f0bd9b71d3ebc3012190205d3e95eee6e
X-lem <regis@bookreport.io>
2025-07-25 21:10:57 -0600

    Add ports

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,12 +3,14 @@ set -euo pipefail
 
 export SERVER_DOMAIN=${SERVER_DOMAIN}
 export AGENT_DOMAIN=${AGENT_DOMAIN}
+export SERVER_PORT=${SERVER_PORT}
+export AGENT_PORT=${AGENT_PORT}
 
 # strip https:// or https:// from domain if necessary
 SERVER_DOMAIN=${SERVER_DOMAIN##*://}
 AGENT_DOMAIN=${AGENT_DOMAIN##*://}
 
-echo using server: ${SERVER_DOMAIN}
-echo using agent: ${AGENT_DOMAIN}
+echo using server: ${SERVER_DOMAIN}:${SERVER_PORT}
+echo using agent: ${AGENT_DOMAIN}:${AGENT_PORT}
 
 exec caddy run --config Caddyfile --adapter caddyfile 2>&1
